a56652c9c1f8e883831c62ee56486ed2391af975
Allie <allie@codefellows.com>
2018-04-10 12:49:38 -0700

    adding table demo code

--- 07-oop-constructors-tables/demo/app.js
+++ 07-oop-constructors-tables/demo/app.js
@@ -25,29 +25,91 @@ Thus, we'll want to create and append HTML elements in the same order.
 We have already put an empty <table> element in the HTML, with id="dogs" */
 
 // We need an array to hold our dogs
+var allDogs = [];
 
 // We need to access the table that is in the DOM
+var dogTable = document.getElementById('dogs');
 
 // We need a constructor to make our dog objects
+function Dog(name, color, breed, nickname) {
+  this.name = name;
+  this.color = color;
+  this.breed = breed;
+  this.nickname = nickname;
+  allDogs.push(this);
+  // storeNames.push(this.storeName);
+}
 
 // Let's make a render() method on the constructor's prototype, rather than typed into the constructor function; this will tidy up the way things look in your editor
-render = function () {
+Dog.prototype.renderRow = function () {
   // create tr
-
+  var trElement = document.createElement('tr');
+  
   // create td
-
+  var tdElement = document.createElement('td');
+  
   // give td content (name, then color, then breed, the nickname) <- eventually do it in a for loop
-
+  tdElement.textContent = this.name;
+  
   // append td to tr
-
+  trElement.appendChild(tdElement);
+  
+  tdElement = document.createElement('td');
+  tdElement.textContent = this.color;
+  trElement.appendChild(tdElement);
+  
+  tdElement = document.createElement('td');
+  tdElement.textContent = this.breed;
+  trElement.appendChild(tdElement);
+  
+  tdElement = document.createElement('td');
+  tdElement.textContent = this.nickname;
+  trElement.appendChild(tdElement);
+  
   // append tr to table
-
+  dogTable.appendChild(trElement);
 };
 
 // We need to create our Dog instances
+// We can assign these to variables if we are calling methods on a specific instance, such as gary.renderRow()
+// Or, we can create an instance beginning with the "new" keyword, but without assigning it to a variable, if we are using the Dog.renderAllDogs method **because we pushed each instance into the allDogs array upon creation**
+var gary = new Dog('Gary', 'White', 'Westie', 'Gare Bear');
+var charlotte = new Dog('Charlotte', 'White', 'Westie', 'Goose');
 
 // We need a separate function to make the table header
+Dog.renderHeader = function() {
+  // create a row
+  var headerRow = document.createElement('tr');
+  
+  var headings = ['Name', 'Color', 'Breed', 'Nickname'];
+  
+  for(var i of headings) {
+    // fill in the header row with th elements
+  // 1. create th elements
+    var thElement = document.createElement('th');
+  
+    // 2. fill in their content
+    // Note, if using "for(var i in headings)" then the following line would be written as: 
+    //    "thElement.textContent = headings[i]"
+    thElement.textContent = i;
+  
+    // 3. append th to headerRow
+    headerRow.appendChild(thElement);
+  }
+  
+  // append to the table
+  dogTable.appendChild(headerRow);
+}
 
 // It would be nice to have a single function that renders all of the individual dog rows
+Dog.renderAllDogs = function() {
+  for(var i in allDogs) {
+    allDogs[i].renderRow();
+  }
+}
 
+Dog.renderHeader();
+Dog.renderAllDogs();
 // Now we need to call our functions: the one for the header row, and the one for generating the individual dog rows
+// gary.renderRow();
+// charlotte.renderRow();
